fef90305c1025c470115a274980bdb8c3357c627
James Smoot <jimi@jsfour.com>
2022-06-16 07:09:19 -0700

    updated logo on mobile

--- src/pages/sections/hero2.tsx
+++ src/pages/sections/hero2.tsx
@@ -52,8 +52,7 @@ const Nav = ({
                 <img
                   className="h-8 w-auto sm:h-10"
                   src={BrandMark}
-                  //src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt=""
+                  alt="Endao Mark"
                 />
               </a>
               <div className="-mr-2 flex items-center md:hidden">
@@ -95,11 +94,7 @@ const Nav = ({
           <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
             <div className="px-5 pt-4 flex items-center justify-between">
               <div>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt=""
-                />
+                <img className="h-8 w-auto" src={BrandMark} alt="Endao Mark" />
               </div>
               <div className="-mr-2">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
